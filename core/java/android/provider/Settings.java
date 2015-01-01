02aba0af557e9dd2f24820d9f2c70aa67fb3a05c
Evisceration <eviscerationls@gmail.com>
2015-01-01 14:29:50 +0000

    [1/2] Quick settings: Add Haptic Feedback to tiles
    Change-Id: I792a00a2689a572110963db00362f8e9d77b316e
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2752,6 +2752,12 @@ public final class Settings {
          */
         public static final String EGG_MODE = "egg_mode";
 
+        /** Whether to vibrate when quick settings tile is pressed.
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
+       
         /**
          * Show or hide clock
          * 0 - hide
