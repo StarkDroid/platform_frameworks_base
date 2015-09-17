476aa88538075ff992a3c8ef0becdd7912f165fa
herriojr <jherriott@cyngn.com>
2015-09-17 13:43:38 -0700

    Default to Resolver Theme
    In order to support being able to dynamically use internal resources
    in CMResolver, we need to actually default to a theme if one isn't
    supplied since whatever is here ends up overriding whatever is defined
    within the Resolver Application.
    
    Change-Id: Id75fafde4208f168577fd29d178443f3c6142954
    (cherry picked from commit 865f39ee5507f39a373198ac00018dc775b7422f)

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -8261,6 +8261,9 @@ public class PackageManagerService extends IPackageManager.Stub {
                     break;
                 }
             }
+            if (mResolveActivity.theme == 0) {
+                mResolveActivity.theme = R.style.Theme_DeviceDefault_Resolver;
+            }
         }
     }
 
